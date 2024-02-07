1128236544bdd1aeef898ba54d99a99c4f655d15
tekeshitime <kyaputenn13@gmail.com>
2024-02-07 18:25:25 +0900

    first commit

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ThreadController extends Controller
+{
+    public function index()
+    {
+        return view('threads.index');
+    }
+
+    public function create()
+    {
+        return view('threads.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'body' => 'required|string|max:512',
+        ]);
+
+        $thread =  DB::transaction(function () use ($request) {
+            $thread = $request->user()->threads()->create([
+                'title' => $request->title,
+            ]);
+
+            $thread->comments()->create([
+                'body' => $request->body,
+                'user_id' => $request->user()->id
+            ]);
+
+            return $thread;
+        });
+
+        return redirect()->route("threads.show", $thread);
+    }
+
+    public function show(Thread $thread)
+    {
+        $comments = $thread->comments()->with(['user'])->paginate(20);
+
+        return view('threads.show', [
+            'thread' => $thread,
+            'comments' => $comments
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThreadController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/threads', [ThreadController::class, 'index'])->name('threads');
+
+Route::get('/threads/create', [ThreadController::class, 'create'])->name('threads.create');
+Route::post('/threads/create', [ThreadController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/threads/{thread}', [ThreadController::class, 'show'])->name('threads.show');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
