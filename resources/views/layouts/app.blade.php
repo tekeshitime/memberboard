d775aa834163c7933814e83f0c5f5749e4cc7005
tekeshitime <kyaputenn13@gmail.com>
2024-02-07 21:57:16 +0900

    index画面仮作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,64 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+        @if (isset($header))
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+        @endif
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+        <footer>
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="flex-shrink-0 flex items-center">
+                        <a href="{{ route('threads') }}">
+                            © 2024 PULSE GARDEN. All Rights Reserved
+                        </a>
                     </div>
-                </header>
-            @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('Terms & Use') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('Policy') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('other') }}
+                        </x-nav-link>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+
 </html>
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -9,6 +9,32 @@
     </x-slot>
 
     <div class="py-12 max-w-4xl mx-auto sm:px-6 lg:px-8 grid gap-y-2">
+        <div class="search">
+            <h2>お気に入りのトラックを見つけよう</h2>
+            <input type="text" placeholder="Explore new sounds">
+            <button type="submit">Search</button>
+        </div>
+    </div>
+
+    <div class="py-12 max-w-4xl mx-auto sm:px-6 lg:px-8 grid gap-y-2 items-center">
+        <div class="flex justify-between items-center">
+            <h2>トレンドのトラック</h2>
+            <span class="text-right">SEE MORE>></span>
+        </div>
+        <div class="flex gap-x-4">
+            @foreach(range(1, 5) as $index)
+            <div class="trackBox">
+                <img src="https://placehold.jp/150x150.png" alt="">
+                <span>￥2000</span>・<span>80BPM</span>
+                <h3 class="trackTitle">Commited</h3>
+                <h4 class="artistName">BLANC</h4>
+            </div>
+            @endforeach
+        </div>
+    </div>
+
+
+    <div class="py-12 max-w-4xl mx-auto sm:px-6 lg:px-8 grid gap-y-2">
         @if($threads->count())
         @foreach($threads as $thread)
         <x-thread-card :thread="$thread" />
@@ -17,4 +43,15 @@
         There is no thread.
         @endif
     </div>
+
+    <div class="py-12 max-w-4xl mx-auto sm:px-6 lg:px-8 grid gap-y-2">
+        <h2>お知らせ</h2>
+        <div>
+            <p>2024.02.01-楽曲○○を追加しました。</p>
+            <p>2024.01.01-サイトを開設しました。</p>
+        </div>
+    </div>
+
+
+
 </x-app-layout>
